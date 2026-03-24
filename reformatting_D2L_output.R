355e1adb0378324ff46ffd2463a38e6b4febd1d7
Kyla Dahlin <kyla.dahlin@gmail.com>
2026-03-24 14:06:37 -0400

    new script for combining D2l .csvs

---
+++ reformatting_D2L_output.R
@@ -0,0 +1,50 @@
+# reminder- to export quizzes in D2L go to Quizzes > Click on the down arrow
+# next to the quiz > Select "Statistics" > Select the "Question Details" tab >
+# select "Export to CSV"
+
+library(tidyr)
+
+setwd("C:/Users/Kyla Dahlin/Dropbox/MSU_GEO201/2026/exam_2/")
+
+files <- list.files("./quizzes")
+
+in.1 <- read.csv(file.path("quizzes", files[1]))
+in.1 <- in.1[c(2,3,5,8,9)]
+names(in.1)[1] <- "Q.num"
+
+wide.1 <- in.1 %>%
+  group_by(Q.num) %>%
+  mutate(
+    answer_col = LETTERS[row_number()],
+    Correct = answer_col[Answer.Match == "Checked"]) %>%
+  ungroup() %>%
+  pivot_wider(
+    id_cols = c(Q.num, Q.Type, Q.Text, Correct),
+    names_from = answer_col,
+    values_from = Answer
+  )
+wide.1$Quiz <- paste0("Quiz_", 1)
+
+out.all <- wide.1
+
+for (i in 2:length(files)) {
+  in.x <- read.csv(file.path("quizzes", files[i]))
+  in.x <- in.x[c(2,3,5,8,9)]
+  names(in.x)[1] <- "Q.num"
+  
+  wide.x <- in.x %>%
+    group_by(Q.num) %>%
+    mutate(
+      answer_col = LETTERS[row_number()],
+      Correct = answer_col[Answer.Match == "Checked"]) %>%
+    ungroup() %>%
+    pivot_wider(
+      id_cols = c(Q.num, Q.Type, Q.Text, Correct),
+      names_from = answer_col,
+      values_from = Answer
+    )
+  wide.x$Quiz <- paste0("Quiz_", i)
+  out.all <- rbind(out.all, wide.x)
+}
+
+write.csv("./quizzes/quizzes_combined.csv")
